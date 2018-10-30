933a39955863d56878b3834fca2ceae45bf88b1c
YeonHoJeong <dusgh2101@naver.com>
2018-10-30 19:58:31 +0900

    query 에러 수정

--- database/test/query.js
+++ database/test/query.js
@@ -1,7 +1,7 @@
 import {connect, transaction} from '../db';
 
 export const connectionTest = connect(async (con) =>{
-    let query = "SELECT * FROM USER "; // 쿼리문 작성시 가급적 맨 뒤에 공백 넣어줄 것
+    let query = "SELECT * FROM user "; // 쿼리문 작성시 가급적 맨 뒤에 공백 넣어줄 것
     const result = await con.query(query, []);  //해당 결과는 변경이 되면 이상하므로 const.
 
     return result;
@@ -9,7 +9,7 @@ export const connectionTest = connect(async (con) =>{
 
 
 export const transactionTest = transaction(async (con) =>{
-    let fetchQuery = "SELECT *FROM USER ";
+    let fetchQuery = "SELECT *FROM user ";
     let insertQuery = "INSERT INTO user (type, name, account, password) values (?, ?, ?, ?) ";
     //만약 해당 컬럼 중 unique한 컬럼이 있다면 추가를 시도 할 때, 중복 시 transaction rollback 이 발생된다.
 
