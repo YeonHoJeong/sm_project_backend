922017877fcf602d84958e5786d73a2ce48bb9df
YeonHoJeong <dusgh2101@naver.com>
2018-11-17 16:56:44 +0900

    postAttachment.url 도 같이 뽑아오도록 수정

--- database/total_feed/query.js
+++ database/total_feed/query.js
@@ -1,9 +1,10 @@
 import {connect, transaction} from '../db';
 
 export const getTotalFeed = connect(async (con) =>{
-    let query = "SELECT post.id as post_id, post.title, COUNT(*) as like_count FROM post " +
+    let query = "SELECT post.id as post_id, post.title, postAttachment.url, COUNT(*) FROM post " +
         "LEFT JOIN `like` ON post.id = `like`.post_id " +
-        "GROUP BY post.id; "; // 쿼리문 작성시 맨 뒤에 공백 넣어줄 것
+        "LEFT JOIN postAttachment ON post.id = postAttachment.post_id " +
+        "GROUP BY post.id, postAttachment.url; "; // 쿼리문 작성시 맨 뒤에 공백 넣어줄 것
     const result = await con.query(query, []);  //해당 결과는 변경이 되면 이상하므로 const.
 
     return result;
